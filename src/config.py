04b51e0b659d351ec27ffaae4af6e68697c95cdd
Moritz Schäfer <mail@moritzs.de>
2016-12-18 16:18:16 +0100

    add notebooks to view results

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                         os.path.pardir))
 DATASET_PATH = os.path.join(BASE_DIR, 'data/msra-td500')
 PATCH_PATH = os.path.join(BASE_DIR, 'data/patches/')
-DICT_PATH = os.path.join(BASE_DIR, 'data/dict.csv')
+DICT_PATH = os.path.join(BASE_DIR, 'data/dict.npy')
 NUM_PATCHES_PER_TEXT = 1  # TODO this should be ~100
 
 NUM_D = 20  # number of dictionary entries

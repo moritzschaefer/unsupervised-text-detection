767b30131a4ad57ff05837e9eadb74e96ab24daa
shansmann <sebastian.hansmann@googlemail.com>
2017-01-05 21:47:18 +0100

    commit

--- src/config.py
+++ src/config.py
@@ -8,5 +8,6 @@ PATCH_PATH = os.path.join(BASE_DIR, 'data/patches/')
 WINDOW_PATH = os.path.join(BASE_DIR, 'data/windows/')
 DICT_PATH = os.path.join(BASE_DIR, 'data/dict.npy')
 NUM_PATCHES_PER_TEXT = 1  # TODO this should be ~100
+ALPHA = .5 #hyperparam from feature extraction
 
 NUM_D = 20  # number of dictionary entries

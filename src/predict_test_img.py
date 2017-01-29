f9b76a5cf78bcb27ae221373e7a2ae89c4fa27ca
Moritz Schäfer <mail@moritzs.de>
2017-01-29 15:18:13 +0100

    change to unix file format. make resizing possible

--- src/predict_test_img.py
+++ src/predict_test_img.py
@@ -15,33 +15,37 @@ dictionary = np.load(config.DICT_PATH)  # get dictionary
 # get all windows of a image
 def sliding_window(img, step_size):
     # windows=[]
-    for x in range(0, img.shape[0], step_size):
-        for y in range(0, img.shape[1], step_size):
-            yield img[x:x + 32, y:y + 32]
+    for y in range(0, img.shape[0], step_size):
+        for x in range(0, img.shape[1], step_size):
+            yield img[y:min(y+32, img.shape[0]), x:min(x+32, img.shape[1])]
             # windows.append(img[x:x+32,y:y+32])
     # return windows
 
 
 # return the value of every pixels of a image
 def get_prediction_values(img, model):
-    values = []
-    for win in sliding_window(img, 1):
-        features = feature_extraction.get_features_for_window(dictionary, win)
-        v = model.predict(features)
-        values.append(v)
-    values = np.array(values).reshape(img.shape)
-    return values
+    layers = []
+    for layer_img in get_all_layers(img):
+        values = []
+        for window in sliding_window(layer_img, 1):
+            features = feature_extraction.get_features_for_window(dictionary,
+                                                                  window)
+            v = model.predict(features)
+            values.append(v)
+        values = np.array(values).reshape(layer_img.shape)
+        layers.append(values)
+    return layers
 
 
 # return all Scaling image of a Image,save into Layer Matrix
 def get_all_layers(img):
-    layer = []
-    for (i, resized) in enumerate(pyramid_gaussian(img, downscale=2)):
+    for (i, resized) in enumerate(pyramid_gaussian(img,
+                                                   downscale=1.3,
+                                                   max_layer=1)):  # TODO use 7
         # if the image is too small, break from the loop
         if resized.shape[0] < 32 or resized.shape[1] < 32:
             break
-        layer.append(resized)
-    return layer
+        yield resized
 
 if __name__ == "__main__":
     model = pickle.load(open(config.TEXT_MODEL_PATH), 'rb')  # get model
@@ -49,25 +53,29 @@ if __name__ == "__main__":
 
     for filename in image_files:
         img = cv2.imread(filename)
-        # layer = get_all_layers(f)  # get all scaling of every image
-        # value_all_layer = []
-        predictions = get_prediction_values(img, model)
+        prediction_layers = get_prediction_values(img, model)
 
         # now draw the image
         text_probability_image = np.zeros(img.shape, np.uint8)
 
         for y in range(0, img.shape[0]):
             for x in range(0, img.shape[1]):
-                window = predictions[max(0, y-32):min(y, predictions.shape[0]),
-                                     max(0, x-32):min(x, predictions.shape[1])]
-                text_probability_image[y, x] = max(window)
+                max_probability = 0
+                for layer in prediction_layers:
+                    # x and y in the layer which correspond to position in
+                    # original image
+                    trans_y = (layer.shape[0]/img.shape[0]) * y
+                    trans_x = (layer.shape[1]/img.shape[1]) * y
+
+                    window = layer[max(0, trans_y-32):
+                                   min(trans_y, layer.shape[0]),
+                                   max(0, trans_x-32):
+                                   min(trans_x, layer.shape[1])]
+
+                    max_probability = max(max_probability, max(window))
+
+                text_probability_image[y, x] = max_probability
 
                 cv2.imshow("image 1", img)
                 cv2.imshow("image 2", text_probability_image)
                 cv2.waitKey(0)
-
-        # TODO: layer
-        # for l in layer:  # for every layer
-        #     # get the value of each pixels in every layer
-        #     value = get_predict_value(f, model)
-        #     value_all_layer.append()  # save value of every layer into value_AllLayer

7ab6ca5b6cd6f36be89f522afb795d3cbf5fe66d
Moritz Schäfer <mail@moritzs.de>
2017-01-29 14:33:52 +0100

    draw probability of pictures

--- src/predict_test_img.py
+++ src/predict_test_img.py
@@ -6,52 +6,68 @@ import os
 import glob
 from skimage.transform import pyramid_gaussian
 import pickle
-from sklearn import svm
 
 
-model=pickle.load(open("tr_model.sav"),'rb')#get model
-dictionary=np.load(dict.npy)#get dictionary
+# TODO use a path from config
+dictionary = np.load(config.DICT_PATH)  # get dictionary
 
-def sliding_window(img,stepSize):#get all windows of a image
-    windows=[]
-    for x in range(0,img.shape[0],stepSize):
-        for y in range(0,img.shape[1],stepSize):
-            windows.append(img[x:x+32,y:y+32])
-    return windows
-        
 
-def get_predictValue(img,model):#return the value of every pixels of a image
-    cv2.load(img)
-    windows=sliding_window(img,1)
-    values=[]
-    for win in windows:
-        features=feature_extraction.get_features_for_window(dictionary, win)
-        v=model.predict(features)
+# get all windows of a image
+def sliding_window(img, step_size):
+    # windows=[]
+    for x in range(0, img.shape[0], step_size):
+        for y in range(0, img.shape[1], step_size):
+            yield img[x:x + 32, y:y + 32]
+            # windows.append(img[x:x+32,y:y+32])
+    # return windows
+
+
+# return the value of every pixels of a image
+def get_prediction_values(img, model):
+    values = []
+    for win in sliding_window(img, 1):
+        features = feature_extraction.get_features_for_window(dictionary, win)
+        v = model.predict(features)
         values.append(v)
-    values=values.reshape(img.shape)
+    values = np.array(values).reshape(img.shape)
     return values
 
-def get_AllLayer(img):#return all Scaling image of a Image,save into Layer Matrix 
-    Layer=[]
+
+# return all Scaling image of a Image,save into Layer Matrix
+def get_all_layers(img):
+    layer = []
     for (i, resized) in enumerate(pyramid_gaussian(img, downscale=2)):
         # if the image is too small, break from the loop
         if resized.shape[0] < 32 or resized.shape[1] < 32:
             break
-        Layer.append(resized)
-    return Layer
+        layer.append(resized)
+    return layer
 
-if __name__=="__main__":
-       
-    image_folders = glob.glob(os.path.join(config.TESTIMAGE_PATH, '*/'))#here should indict exactly path where test image file store
-    
-    for folder in image_folders:
-    
-        image_files = glob.glob(os.path.join(folder, '*.jpg'))
-    
-        for f in image_files:
-            Layer=get_AllLayer(f)#get all scaling of every image
-            value_AllLayer=[]
-            for l in Layer:#for every layer
-                value=get_predictValue(f,model)#get the value of each pixels in every layer
-                value_AllLayer.append()#save value of every layer into value_AllLayer
+if __name__ == "__main__":
+    model = pickle.load(open(config.TEXT_MODEL_PATH), 'rb')  # get model
+    image_files = glob.glob(os.path.join(config.TEST_IMAGE_PATH, '/*.jpg'))
 
+    for filename in image_files:
+        img = cv2.imread(filename)
+        # layer = get_all_layers(f)  # get all scaling of every image
+        # value_all_layer = []
+        predictions = get_prediction_values(img, model)
+
+        # now draw the image
+        text_probability_image = np.zeros(img.shape, np.uint8)
+
+        for y in range(0, img.shape[0]):
+            for x in range(0, img.shape[1]):
+                window = predictions[max(0, y-32):min(y, predictions.shape[0]),
+                                     max(0, x-32):min(x, predictions.shape[1])]
+                text_probability_image[y, x] = max(window)
+
+                cv2.imshow("image 1", img)
+                cv2.imshow("image 2", text_probability_image)
+                cv2.waitKey(0)
+
+        # TODO: layer
+        # for l in layer:  # for every layer
+        #     # get the value of each pixels in every layer
+        #     value = get_predict_value(f, model)
+        #     value_all_layer.append()  # save value of every layer into value_AllLayer
